e29488621a5930b974cbc0a2a365e1771671a783
Carolina Perez-Vargas <cpvp20@gmail.com>
2019-07-05 16:08:17 -0500

    add, addi, sub, or, ori, and, andi, lui, nor, sll, srl, lw, sw, beq, bne, j, jal, jr

--- ALUControl.v
+++ ALUControl.v
@@ -14,46 +14,55 @@
 ******************************************************************/
 module ALUControl
 (
-	input [2:0] ALUOp,
+	input [3:0] ALUOp,
 	input [5:0] ALUFunction,
 	output [3:0] ALUOperation
 
 );
 
-localparam R_Type_SLL    = 9'b111_000000;
-localparam R_Type_SRL    = 9'b111_000010;
-localparam R_Type_AND    = 9'b111_100100;
-localparam R_Type_OR     = 9'b111_100101;
-localparam R_Type_NOR    = 9'b111_100111;
-localparam R_Type_ADD    = 9'b111_100000;
-localparam I_Type_ADDI   = 9'b100_xxxxxx;
-localparam I_Type_ORI    = 9'b101_xxxxxx;
-localparam I_Type_ANDI   = 9'b110_xxxxxx;
-localparam I_Type_LUI   = 9'b111_xxxxxx;
-
-
+localparam R_Type_AND  = 10'b0111_100100;
+localparam R_Type_OR   = 10'b0111_100101;
+localparam R_Type_NOR  = 10'b0111_100111;
+localparam R_Type_ADD  = 10'b0111_100000;
+localparam R_Type_SUB  = 10'b0111_100010;
+localparam R_Type_SLL  = 10'b0111_000000;
+localparam R_Type_SRL  = 10'b0111_000010;
+localparam R_Type_JR   = 10'b0111_001000;
+localparam I_Type_ADDI = 10'b0100_xxxxxx;
+localparam I_Type_ANDI = 10'b0101_xxxxxx;
+localparam I_Type_ORI  = 10'b0110_xxxxxx;
+localparam I_Type_LUI  = 10'b0011_xxxxxx;
+localparam I_Type_BEQ  = 10'b1000_xxxxxx;
+localparam I_Type_BNE  = 10'b1001_xxxxxx;
+localparam I_Type_LW   = 10'b1010_xxxxxx;
+localparam I_Type_SW   = 10'b1011_xxxxxx;
 
 
 reg [3:0] ALUControlValues;
-wire [8:0] Selector;
+wire [9:0] Selector;
 
 assign Selector = {ALUOp, ALUFunction};
 
 always@(Selector)begin
 	casex(Selector)
-		R_Type_SLL:    ALUControlValues = 4'b0110;	//6
-		R_Type_SRL:    ALUControlValues = 4'b0111;	//7
-		R_Type_AND:    ALUControlValues = 4'b0000;
-		R_Type_OR: 		ALUControlValues = 4'b0001;
-		R_Type_NOR: 	ALUControlValues = 4'b0010;
-		R_Type_ADD: 	ALUControlValues = 4'b0011;
-		I_Type_ADDI: 	ALUControlValues = 4'b0011;
-		I_Type_ORI: 	ALUControlValues = 4'b0001;
-		I_Type_ANDI: 	ALUControlValues = 4'b0000;
-		I_Type_LUI: 	ALUControlValues = 4'b0101;
-
+		R_Type_AND : ALUControlValues = 4'b0000;
+      R_Type_OR  : ALUControlValues = 4'b0001;
+      R_Type_NOR : ALUControlValues = 4'b0010;
+      R_Type_ADD : ALUControlValues = 4'b0011;	//add
+      R_Type_SUB : ALUControlValues = 4'b1001;	//sub
+      R_Type_SLL : ALUControlValues = 4'b0100;
+      R_Type_SRL : ALUControlValues = 4'b0101;
+      I_Type_ADDI: ALUControlValues = 4'b0011;	//add
+      I_Type_ORI : ALUControlValues = 4'b0001;
+      I_Type_LUI : ALUControlValues = 4'b1000;
+      I_Type_ANDI: ALUControlValues = 4'b0000;
+      I_Type_BEQ : ALUControlValues = 4'b1001;	//sub
+      I_Type_BNE : ALUControlValues = 4'b1001;	//sub
+      I_Type_LW  : ALUControlValues = 4'b0011;	//add
+      I_Type_SW  : ALUControlValues = 4'b0011;	//add
 		
-		default: ALUControlValues = 4'b1001;
+		
+		default: ALUControlValues = 4'b1111;
 	endcase
 end
 
