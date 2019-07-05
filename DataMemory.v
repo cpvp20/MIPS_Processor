9fb79a5518490cd9443bee19a74e2af30141821c
Carolina Perez-Vargas <cpvp20@gmail.com>
2019-07-05 16:14:18 -0500

    NA

--- DataMemory.v
+++ DataMemory.v
@@ -1,19 +1,12 @@
 /******************************************************************
 * Description
 *	This is the data memory for the MIPS processor
-*	1.0
-* Author:
-*	Dr. José Luis Pizano Escalante
-* email:
-*	luispizano@iteso.mx
-* Date:
-*	01/03/2014
+
 ******************************************************************/
 
 module DataMemory 
-#(	parameter DATA_WIDTH=8,
+#(	parameter DATA_WIDTH=32,
 	parameter MEMORY_DEPTH = 1024
-
 )
 (
 	input [DATA_WIDTH-1:0] WriteData,
